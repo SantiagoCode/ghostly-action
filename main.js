092157e52de4ba9ea13e6cc933ab9a640083f74d
Santiago Salazar <santiagosalazar.dev@gmail.com>
2024-03-25 22:02:03 -0400

    Some advances, but still broken

--- main.js
+++ main.js
@@ -17,40 +17,8 @@ const getPosition = () => {
   return { divPosX, divPosY }
 }
 
-const updateAnotherDiv = () => {
-  if(position == 1) {
-    const el = document.querySelector('#secondary')
-    const data = localStorage.getItem('B')
-
-    const { windowX, windowY } = JSON.parse(data)
-
-    el.style.top = `${Number( (window.outerHeight/2) + windowY )}px`
-    el.style.left = `${Number( (window.outerWidth/2) + windowX )}px`
-
-    console.log(windowX, windowY);
-  }
-}
-
-const startGame = () => {
-  localStorage.setItem("init", true)
-  document.querySelector('#app').innerHTML = `<div id="${position == 1 ? 'principal' : 'secondary'}" class="block ${position == 1 ? 'blue' : 'red'}"></div>`
-  document.querySelector('#app').innerHTML += `<div id="${position == 0 ? 'principal' : 'secondary'}" class="block ${position == 0 ? 'blue' : 'red'}"></div>`
-
-  if (position == 1) {
-    document.querySelector('#principal').classList.add('fixed')
-  } else {
-    document.querySelector('#secondary').classList.add('fixed')
-  }
-
-  setLocalStorage()
-  setInterval(() => updateAnotherDiv(), 1000)
-}
-
 const setLocalStorage = () => {
   const { divPosX, divPosY } = getPosition()
-  
-  console.log(position);
-  console.log('setLocalStorage... ', { divPosX, divPosY });
 
   localStorage.setItem(position == 1 ? 'A' : 'B', JSON.stringify({
     id: position,
@@ -61,17 +29,62 @@ const setLocalStorage = () => {
   setTimeout(() => setLocalStorage(), 1000)
 }
 
+const setStyles = ({ el, data }) => {
+  const { windowX, windowY } = JSON.parse(data)
+
+  // Obtener las dimensiones del elemento div
+  const divWidth = el.offsetWidth;
+  const divHeight = el.offsetHeight;
+
+  el.style.top = `${Number(windowY - (window.outerHeight/2) + (divHeight / 2))}px`
+  el.style.left = `${Number(windowX - (window.outerWidth/2) + (divWidth / 2))}px`
+}
+
+const updateElement = () => {
+  if(position == 1) {
+    const el = document.querySelector('#secondary')
+    const data = localStorage.getItem('B')
+
+    setStyles({ el, data })
+  } else {
+    const el = document.querySelector('#principal')
+    const data = localStorage.getItem('A')
+    
+    setStyles({ el, data })
+  }
+}
+
+const startGame = () => {
+  localStorage.setItem("init", true)
+
+  document.querySelector('#app').innerHTML = `<div id="${position == 1 ? 'principal' : 'secondary'}" class="block ${position == 1 ? 'blue' : 'red'}"></div>`
+  document.querySelector('#app').innerHTML += `<div id="${position == 0 ? 'principal' : 'secondary'}" class="block ${position == 0 ? 'blue' : 'red'}"></div>`
+
+  if (position == 1) {
+    document.querySelector('#principal').classList.add('fixed')
+  } else {
+    document.querySelector('#secondary').classList.add('fixed')
+  }
+
+  setLocalStorage()
+  setInterval(() => updateElement(), 1000)
+}
+
+// PROGRAM INITIALIZATION
 if(localStorage.getItem("init")) {
   startGame()
 } else {
   document.querySelector('#openNewWindow').addEventListener('click', () => {
     position = 1
     startGame()
-    openSecondWindow()
+    openNewWindow()
+
+    window.addEventListener('storage', () => updateElement())
   })
 }
 
-const openSecondWindow = () => {
+// OPEN NEW WINDOW
+const openNewWindow = () => {
   const currentUrl = window.location.href
   window.open(currentUrl, '_blank')
 }
